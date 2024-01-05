510da975e90687f6ea703ee9cf14c06c7f9e39de
MoiBaguette <gerco.v.woudenbergh@gmail.com>
2024-01-05 22:07:28 +0100

    loader and counter

--- rabbithole.py
+++ rabbithole.py
@@ -1,2 +1,9 @@
 #this programs lists words that are uncommon in a bulgarian text
 #This is too help study for a text so when reading it will be a more enjoyable process
+from load_data import Load
+from collections import Counter
+
+Loader = Load("tatoeba_bg_eng.tsv")
+bg_words = Loader.bg_words
+counts = Counter(bg_words)
+print(counts)
